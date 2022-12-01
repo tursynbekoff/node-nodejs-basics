7891aa0d5ec1166d68c11a5f587045fa5c6b3fa0
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-12-01 13:51:28 +0100

    adding file creation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    if (fs.existsSync('src/cp/files/fresh.txt')) {
+        throw 'FS operation failed'
+    } 
+
+    fs.writeFileSync('src/cp/files/fresh.txt','I am fresh and young', (err) => {
+        if(err) throw err;
+    });
+
 };
 
 spawnChildProcess();
--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,6 @@
 const copy = async () => {
     // Write your code here 
+    console.log('copying file')
 };
 
 copy();
