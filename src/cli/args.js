f6f1f6a0a4c35b817aad6c2d2a259832a25f4b0a
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-12-02 16:21:37 +0100

    cli implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    const args = process.argv;
+
+    const startIndex = args.findIndex((e) => {
+        return e.includes('--');
+    })
+    
+    const arr = args.slice(startIndex, args.length);
+
+    arr.forEach(function(item, index) {
+        if(index % 2 === 0) {
+           console.log(arr[index].replace('--', ''), 'is', arr[index + 1]);
+        }
+    });
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
-    
+
+    let no_env = 0;
+
+    const env = process.env;
+
+    for (let key in env) {
+        if (key.includes('RSS_')) {
+            console.log(key + "=" + env[key]);
+        }   
+        no_env++;
+    }
+
 };
 
 parseEnv();
