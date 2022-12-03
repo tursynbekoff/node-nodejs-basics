4e86fc1104a4501e4ecc30a61c2a5457e2a9393f
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-12-03 16:39:39 +0100

    worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
     // Write your code here
+
+    const worker = new Worker('./src/wt/worker.js');
+
+    worker.on('message', result => {
+        console.log(`status - resolved, data - ${result.fib}`);
+    })
+
+    worker.on('error', error => {
+        console.log('error', error);
+    })
+    worker.postMessage({num: 10});
+    worker.postMessage({num: 11});
+    worker.postMessage({num: 12});
+    worker.postMessage({num: 13});
+
 };
 
 await performCalculations();
